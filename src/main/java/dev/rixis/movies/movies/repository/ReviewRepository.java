859cb94f5141976c88eeb56f4f0916ec451d67f8
Konrad Sygut <konrsyg825@student.polsl.pl>
2023-02-24 02:17:33 +0100

    Review methods implemented

---
+++ src/main/java/dev/rixis/movies/movies/repository/ReviewRepository.java
@@ -0,0 +1,11 @@
+package dev.rixis.movies.movies.repository;
+
+import dev.rixis.movies.movies.model.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+}
---
+++ src/main/java/dev/rixis/movies/movies/service/ReviewService.java
@@ -0,0 +1,34 @@
+package dev.rixis.movies.movies.service;
+
+import dev.rixis.movies.movies.repository.ReviewRepository;
+import dev.rixis.movies.movies.model.Movie;
+import dev.rixis.movies.movies.model.Review;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
+    private final MongoTemplate mongoTemplate;
+
+    public ReviewService(ReviewRepository reviewRepository, MongoTemplate mongoTemplate) {
+        this.reviewRepository = reviewRepository;
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    public Review createReview(String reviewBody, String imdbId){
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+        return review;
+    }
+
+
+}
