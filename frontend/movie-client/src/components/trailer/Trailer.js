bb717339a87ac57b31837f0d7fd0812337fb0f70
Konrad Sygut <konrsyg825@student.polsl.pl>
2023-02-27 02:22:19 +0100

    Trailer component done

---
+++ frontend/movie-client/src/components/trailer/Trailer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {useParams} from "react-router-dom";
+import ReactPlayer from "react-player";
+import "Trailer.css"
+
+const Trailer = () => {
+    return (
+        <div className="react-player-container">
+
+        </div>
+    );
+};
+
+export default Trailer;
